43d14068c843bdf24b240045e7d21d46de4908ff
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-29 15:06:06 +0500

    Step05- Create Landning and error component
    a:- Created
    	i:- src/components/landing
    		1:- Landing.tsx
    		2:- landing.css
    	ii:- src/components/errorPage
    		1:- ErrorPage.tsx
    		2:- errorPage.css
    b:- updated
    	i:- src.pages/
    		1:- index.tsx
    		2:- 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import Footer from "../components/addOns/Footer";
+import Header from "../components/addOns/Header";
+import { ErrorPage } from "../components/errorPage/ErrorPage";
 
 export default function Error() {
-  return <div>Error Page</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Virtual Lolly</title>
+      </Helmet>
+      <Header />
+      <ErrorPage />
+      <Footer />
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Footer from "../components/addOns/Footer";
 import Header from "../components/addOns/Header";
+import { Landing } from "../components/landing/Landing";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <title>Virtual Lolly</title>
       </Helmet>
       <Header />
-      <div>Home Page</div>
+      <Landing />
       <Footer />
     </div>
   );
